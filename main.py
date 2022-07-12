c4a8f27e772cd12a1dc502f2b425f0110ee70b95
Olcan Simsek <Olcan.Simsek@sahibinden.com>
2022-07-12 22:59:00 +0300

    exampels added

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+# classes define the state and the behavior of the object
+
+
+class House:
+    def __init__(self, price):  # init method of the class
+        self.price = price      # first price is the attribute of the instance, self refers to the instance,
+
+
 class Backpack:
-    pass
+    def __init__(self, color, size):
+        self.items = []         # items attribute for backpack is empty list
+        self.color = color
+        self.size = size
